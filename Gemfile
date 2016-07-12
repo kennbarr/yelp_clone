6282409606f7aebd4a7226d4826016ea66c81fab
Kenneth Barrett <barrett.kenneth@gmail.com>
2016-07-12 17:10:36 +0100

    logging in with facebook enabled

--- Gemfile
+++ Gemfile
@@ -9,7 +9,8 @@ gem 'coffee-rails', '~> 4.2'
 gem 'jquery-rails'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
-
+gem 'devise'
+gem 'omniauth-facebook'
 
 group :test do
   gem 'rspec-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :restaurants do
     resources :reviews
   end
+
+  root "restaurants#index"
 
   # get 'restaurants' => 'restaurants#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
