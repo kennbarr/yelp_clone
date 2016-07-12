026f11f7a152e520c1d8410d651e2e15d9d1cd61
Anna <anna.hollandsmith3@gmail.com>
2016-07-12 09:18:40 +0100

    changes to controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,7 +9,7 @@ class RestaurantsController < ApplicationController
 
   def create
     Restaurant.create(restaurant_params)
-    redirect_to '/restaurants'
+    redirect_to restaurants_path
   end
 
   def show
