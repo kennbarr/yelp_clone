ca76d082f3a665c1b9526ed2f8ca26f7a08ec115
Kenneth Barrett <barrett.kenneth@gmail.com>
2016-07-13 11:04:41 +0100

    tweaked

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,13 +11,5 @@ class User < ApplicationRecord
      user.password = Devise.friendly_token[0,20]
    end
 
-  #  def self.new_with_session(params, session)
-  #   super.tap do |user|
-  #     if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-  #       user.email = data["email"] if user.email.blank?
-  #     end
-  #   end
-  # end
-
   end
 end
